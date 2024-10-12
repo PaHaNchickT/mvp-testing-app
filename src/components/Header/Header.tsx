aca76b4d572030fb41ada7f900c1999f679363e2
Pavel <you@example.com>
2024-10-13 00:40:21 +0300

    feat: implement styles for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ import { TEXT_CONTENT } from '@/constants/TEXT_CONTENT';
 
 const Header = (): ReactElement => {
   return (
-    <header className="h-[70px] bg-[#c0c0c0] flex justify-center items-center">
+    <header className="h-[70px] bg-[#c0c0c0] flex justify-center items-center font-black">
       <h1 className="text-xl sm:text-3xl">{TEXT_CONTENT.header.headerTitle}</h1>
     </header>
   );
