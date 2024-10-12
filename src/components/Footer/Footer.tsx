80e6713610a0f3207852129f5ef5b665462a3a7b
Pavel <you@example.com>
2024-10-13 00:37:18 +0300

    fix: change footer font-weight

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ import { TEXT_CONTENT } from '@/constants/TEXT_CONTENT';
 
 export const Footer = (): ReactElement => {
   return (
-    <footer className="bg-[#c0c0c0] gap-2 flex flex-col flex-wrap justify-center items-center text-xs p-5 relative md:text-sm sm:flex-row sm:justify-between sm:flex-row">
+    <footer className="font-medium bg-[#c0c0c0] gap-2 flex flex-col flex-wrap justify-center items-center text-xs p-5 relative md:text-sm sm:flex-row sm:justify-between sm:flex-row">
       <p>{TEXT_CONTENT.footer.madeby}</p>
       <p className=" w-[62px]  relative left-0 sm:absolute sm:text-center sm:left-[calc((100%-62px)/2)]">2024</p>
       <Link href="https://ternopavel.ru/" underline="hover" target="_blank" size="sm">
