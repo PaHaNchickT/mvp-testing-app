d198cdfc8cf95f6f8ce96cd8622fd1ca25ddbf39
Pavel <you@example.com>
2024-10-13 16:48:41 +0300

    feat: implement responsive mode for footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,9 +7,9 @@ import { TEXT_CONTENT } from '@/constants/TEXT_CONTENT';
 
 export const Footer = (): ReactElement => {
   return (
-    <footer className="font-medium bg-[#c0c0c0] gap-2 flex flex-col flex-wrap justify-center items-center text-xs p-5 relative md:text-sm sm:flex-row sm:justify-between sm:flex-row">
+    <footer className="font-medium bg-[#c0c0c0] gap-2 flex flex-col flex-wrap justify-center items-center text-sm p-5 relative md:flex-row md:justify-between md:flex-row">
       <p>{TEXT_CONTENT.footer.madeby}</p>
-      <p className=" w-[62px]  relative left-0 sm:absolute sm:text-center sm:left-[calc((100%-62px)/2)]">2024</p>
+      <p className=" relative left-0 md:absolute md:text-center md:left-[calc((100%-62px)/2)] ms:w-[62px]">2024</p>
       <Link href="https://ternopavel.ru/" underline="hover" target="_blank" size="sm">
         {TEXT_CONTENT.footer.works}
       </Link>
