da95d0ca73d15a26f5f11babe6b50970cee41479
Pavel <you@example.com>
2024-10-13 16:37:06 +0300

    feat: implement responsive mode for welcome window

--- src/components/WelcomeWindow/WelcomeWindow.tsx
+++ src/components/WelcomeWindow/WelcomeWindow.tsx
@@ -24,12 +24,12 @@ const WelcomeWindow = (): ReactElement => {
   }, []);
 
   return (
-    <section className={`flex justify-center gap-5 h-[400px] ${opacity} transition-all`}>
-      <Image alt="nextui logo" height="100%" radius="sm" src={ASSETS_PATH.mainImage} />
-      <Divider orientation="vertical" />
-      <div className="w-1/3 flex flex-col gap-5 justify-center items-start">
-        <h2 className="font-black text-2xl">{TEXT_CONTENT.welcome.title}</h2>
-        <p className="text-sm">{TEXT_CONTENT.welcome.text}</p>
+    <section className={`flex justify-center pr-5 gap-5 h-[400px] ${opacity} transition-all md:pr-0`}>
+      <Image alt="nextui logo" height="100%" radius="sm" src={ASSETS_PATH.mainImage} className="hidden md:block" />
+      <Divider orientation="vertical" className="hidden md:block" />
+      <div className="w-full flex flex-col gap-5 justify-center items-center text-center md:text-start md:w-1/3 md:items-start">
+        <h2 className="font-black text-xl w-full lg:text-2xl">{TEXT_CONTENT.welcome.title}</h2>
+        <p className="text-sm w-full">{TEXT_CONTENT.welcome.text}</p>
         <Button color="danger" onPress={clickHandler}>
           {TEXT_CONTENT.welcome.button}
         </Button>
