1ab7142af40afd1449a0dee0e2fd25428922ab9e
Pavel <you@example.com>
2024-10-13 16:42:59 +0300

    feat: implement responsive mode for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,8 +6,8 @@ import { TEXT_CONTENT } from '@/constants/TEXT_CONTENT';
 
 const Header = (): ReactElement => {
   return (
-    <header className="h-[70px] bg-[#c0c0c0] flex justify-center items-center font-black">
-      <h1 className="text-xl sm:text-3xl">{TEXT_CONTENT.header.headerTitle}</h1>
+    <header className="h-[60px] bg-[#c0c0c0] flex justify-center items-center font-black">
+      <h1 className="text-2xl sm:text-3xl">{TEXT_CONTENT.header.headerTitle}</h1>
     </header>
   );
 };
